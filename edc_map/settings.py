f87f586a01661ec6bb82ec2131d3b95ac1f67538
erikvw <ew2789@gmail.com>
2017-02-23 23:00:16 +0200

    update namespace

--- edc_map/settings.py
+++ edc_map/settings.py
@@ -147,7 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
 
-# edc-map
+# edc_map
 GPS_FILE_NAME = '/Volumes/GARMIN/GPX/temp.gpx'
 GPS_DEVICE = '/Volumes/GARMIN/'
 GPX_TEMPLATE = os.path.join(STATIC_ROOT, 'edc_map/gpx/template.gpx')
